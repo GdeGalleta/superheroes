927c9af42e5e09e026647587ddc434d703e81ef4
Dimitri Sopov <dimitri.sopov@gmail.com>
2022-03-05 10:25:56 +0100

    App creation

---
+++ superheroes/superheroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Superheroes
+//
+//  Created by Dimitri Sopov on 5/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
