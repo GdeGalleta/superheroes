c516034b62ff856dada08e0b5a78a879bc77c244
Dimitri Sopov <dimitri.sopov@gmail.com>
2022-03-07 14:56:05 +0100

    Request POST added

--- superheroes/superheroes/Core/Communication/ApiResource.swift
+++ superheroes/superheroes/Core/Communication/ApiResource.swift
@@ -15,30 +15,69 @@ public protocol ApiResourceType: AnyObject {
 public class ApiResource<T: Decodable>: ApiResourceType {
     public typealias Model = T
 
+    public enum HttpMethod: String {
+        case GET
+        case POST
+    }
+
+    let httpMethod: HttpMethod
     let baseURL: String
     let pathURL: String
     let queryParameters: [String: Any]
+    let headers: [String: String]
 
     public var request: URLRequest? {
-        guard !baseURL.isEmpty else {
-            return nil
+        switch httpMethod {
+            case .GET:
+                return requestGET
+            case .POST:
+                return requestPOST
         }
+    }
+
+    private var requestGET: URLRequest? {
         let path = "\(baseURL)\(pathURL)"
+
         var components = URLComponents(string: path)
         components?.queryItems = queryParameters
             .compactMapValues { String(describing: $0) }
             .map { (key, value) in return URLQueryItem(name: key, value: value) }
-        if let url = components?.url?.absoluteURL {
-            return URLRequest(url: url)
+
+        guard let finalURL = components?.url?.absoluteURL else {
+            return nil
         }
-        return nil
+
+        var request = URLRequest(url: finalURL)
+        request.allHTTPHeaderFields = headers
+        request.httpMethod = httpMethod.rawValue
+        return request
     }
 
-    public init(baseURL: String,
+    private var requestPOST: URLRequest? {
+        guard !baseURL.isEmpty else {
+            return nil
+        }
+
+        guard let finalURL = URLComponents(string: "\(baseURL)\(pathURL)")?.url else {
+            return nil
+        }
+
+        var request = URLRequest(url: finalURL)
+        request.allHTTPHeaderFields = headers
+        request.httpMethod = httpMethod.rawValue
+        request.httpBody = try? JSONSerialization.data(withJSONObject: queryParameters, options: [])
+        return request
+    }
+
+    public init(httpMethod: HttpMethod = .GET,
+                baseURL: String,
                 pathURL: String,
-                queryParameters: [String: Any] = [:]) {
+                queryParameters: [String: Any] = [:],
+                headers: [String: String] = [:]) {
+        self.httpMethod = httpMethod
         self.baseURL = baseURL
         self.pathURL = pathURL
         self.queryParameters = queryParameters
+        self.headers = headers
     }
 }
